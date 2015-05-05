9ec8db5a633bb157aa1f2cc8d9ce1b0c5ea62431
Matheus Brasil <matheus.brasil10@gmail.com>
2015-05-05 02:12:43 -0300

    Update controllers
    Update controllers to use the AngularJS services previously defined

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,21 +14,40 @@ angular.module('confboilerplate.controllers', [])
     lng: -38.483531
   };
 
-  $scope.$on('mapInitialized', function(event, map) {
+  $scope.$on('mapInitialized', function(attraction, map) {
     $scope.map = map;
   });
 })
 
-.controller('SpeakersCtrl', function($scope) {
+.controller('SpeakersCtrl', function($scope, $http, Speakers) {
+  $scope.speakers = [];
 
+  Speakers.get()
+  .then(function(speakers){
+    $scope.speakers = speakers;
+  },function(err){
+  });
 })
 
-.controller('ScheduleCtrl', function($scope) {
+.controller('ScheduleCtrl', function($scope, Schedule) {
+  $scope.attractions = [];
 
+  Schedule.get()
+  .then(function(attractions){
+    $scope.attractions = attractions;
+  },function(err){
+  });
 })
 
-.controller('AttractionCtrl', function($scope, $stateParams) {
-  $scope.attractionId = $stateParams.attractionId;
+.controller('AttractionCtrl', function($scope, Schedule, $stateParams) {
+  var attractionId = $stateParams.attractionId;
+
+  Schedule.getAttraction(attractionId)
+  .then(function(attraction){
+    $scope.attraction = attraction;
+  },function(err){
+  });
+
 })
 
 .controller('SponsorsCtrl', function($scope) {

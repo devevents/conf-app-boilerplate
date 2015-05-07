6331bbae77266095e80fae157709594bbc0ed83f
Matheus Brasil <matheus.brasil10@gmail.com>
2015-05-07 15:14:08 -0300

    Add Ionic loader and social sharing

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,34 +19,71 @@ angular.module('confboilerplate.controllers', [])
   });
 })
 
-.controller('SpeakersCtrl', function($scope, $http, Speakers) {
+.controller('SpeakersCtrl', function($scope, $http, Speakers, $ionicLoading) {
   $scope.speakers = [];
+
+  $ionicLoading.show({
+    template: 'Loading...'
+  });
 
   Speakers.get()
   .then(function(speakers){
     $scope.speakers = speakers;
+    $ionicLoading.hide();
   },function(err){
+    $ionicLoading.hide();
   });
+
+  $scope.goToUrl = function(url){
+    //use inAppBrowser plugin
+    window.open(url, '_blank', 'location=yes');
+  }
 })
 
-.controller('ScheduleCtrl', function($scope, Schedule) {
+.controller('ScheduleCtrl', function($scope, Schedule, $ionicLoading) {
   $scope.attractions = [];
+
+  $ionicLoading.show({
+    template: 'Loading...'
+  });
 
   Schedule.get()
   .then(function(attractions){
     $scope.attractions = attractions;
+    $ionicLoading.hide();
   },function(err){
+    $ionicLoading.hide();
   });
 })
 
-.controller('AttractionCtrl', function($scope, Schedule, $stateParams) {
+.controller('AttractionCtrl', function($scope, Schedule, $stateParams, $ionicLoading) {
   var attractionId = $stateParams.attractionId;
+
+  $ionicLoading.show({
+    template: 'Loading...'
+  });
 
   Schedule.getAttraction(attractionId)
   .then(function(attraction){
     $scope.attraction = attraction;
+    $ionicLoading.hide();
   },function(err){
+    $ionicLoading.hide();
   });
+
+  $scope.shareAttraction = function(attraction){
+    var speakersText = "";
+
+    _.each(attraction.speakers, function(speaker, index){
+      speakersText += speaker.name;
+      if((index+1) < attraction.speakers.length){
+        speakersText += " & ";
+      }
+    });
+
+    var message = "Enjoying " + attraction.title + " by " + speakersText + " at #MyConf";
+    window.plugins.socialsharing.share(message);
+  };
 
 })
 
@@ -56,6 +93,4 @@ angular.module('confboilerplate.controllers', [])
 
 .controller('PartnersCtrl', function($scope) {
 
-})
-
-;
+});

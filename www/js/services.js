def2d6c305001c56fe56d19e3e75dbc37e1936cd
Matheus Brasil <matheus.brasil10@gmail.com>
2015-05-08 17:50:44 -0300

    Add a service to fetch data from sponsors.json

--- www/js/services.js
+++ www/js/services.js
@@ -17,6 +17,23 @@ angular.module('confboilerplate.services', [])
   };
 })
 
+.service('Sponsors', function ($http, $q){
+
+  this.get = function() {
+    var dfd = $q.defer();
+
+    $http.get('http://devevents.github.io/conf-app-boilerplate/data/sponsors.json')
+    .success(function(data) {
+      dfd.resolve(data);
+    })
+    .error(function(data) {
+      dfd.reject(data);
+    });
+
+    return dfd.promise;
+  };
+})
+
 .service('Schedule', function ($http, $q){
 
   this.get = function() {

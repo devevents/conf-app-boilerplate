43ec0dd832fb9ba7e348efdd4fbac8620f4e2ce5
Matheus Brasil <matheus.brasil10@gmail.com>
2015-05-08 23:11:24 -0300

    Add Sponsors controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -87,8 +87,25 @@ angular.module('confboilerplate.controllers', [])
 
 })
 
-.controller('SponsorsCtrl', function($scope) {
+.controller('SponsorsCtrl', function($scope, $http, Sponsors, $ionicLoading) {
+  $scope.sponsors = [];
 
+  $ionicLoading.show({
+    template: 'Loading...'
+  });
+
+  Sponsors.get()
+  .then(function(sponsors){
+    $scope.sponsors = sponsors;
+    $ionicLoading.hide();
+  },function(err){
+    $ionicLoading.hide();
+  });
+
+  $scope.goToUrl = function(url){
+    //use inAppBrowser plugin
+    window.open(url, '_blank', 'location=yes');
+  }
 })
 
 .controller('PartnersCtrl', function($scope) {

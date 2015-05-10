e44d7c3bee4c557864bbe9ac5b57b13ef0ccb8f2
Felipe Sousa <felizbr@gmail.com>
2015-05-10 15:28:48 -0300

    add services(about), controller(about) and edit about.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,24 @@ angular.module('confboilerplate.controllers', [])
 
 })
 
-.controller('AboutCtrl', function($scope) {
+.controller('AboutCtrl', function($scope, $http, About, $ionicLoading) {
+  $scope.about = [];
+  $ionicLoading.show({
+    template: 'Loading...'
+  });
 
+  About.get()
+  .then(function(about){
+    $scope.about = about;
+    $ionicLoading.hide();
+  },function(err){
+    $ionicLoading.hide();
+  });
+
+  $scope.goToUrl = function(url){
+    //use inAppBrowser plugin
+    window.open(url, '_blank', 'location=yes');
+  }
 })
 
 .controller('LocationCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,23 @@
 angular.module('confboilerplate.services', [])
 
+
+.service('About', function ($http, $q){
+
+  this.get = function() {
+    var dfd = $q.defer();
+
+    $http.get('http://devevents.github.io/conf-app-boilerplate/data/about.json')
+    .success(function(data) {
+      dfd.resolve(data);
+    })
+    .error(function(data) {
+      dfd.reject(data);
+    });
+
+    return dfd.promise;
+  };
+})
+
 .service('Speakers', function ($http, $q){
 
   this.get = function() {

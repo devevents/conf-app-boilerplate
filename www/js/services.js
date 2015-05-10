372f47180d0de97ed9d25deee4875a2cd72b2d24
Matheus Brasil <matheus.brasil10@gmail.com>
2015-05-10 01:45:50 -0300

    Add a service to fetch data from partners.json

--- www/js/services.js
+++ www/js/services.js
@@ -43,6 +43,23 @@ angular.module('confboilerplate.services', [])
   };
 })
 
+.service('Partners', function ($http, $q){
+
+  this.get = function() {
+    var dfd = $q.defer();
+
+    $http.get('http://devevents.github.io/conf-app-boilerplate/data/partners.json')
+    .success(function(data) {
+      dfd.resolve(data);
+    })
+    .error(function(data) {
+      dfd.reject(data);
+    });
+
+    return dfd.promise;
+  };
+})
+
 .service('Schedule', function ($http, $q){
 
   this.get = function() {

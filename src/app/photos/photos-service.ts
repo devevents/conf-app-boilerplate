ac2255a84c895c7b0b4883128231c97166dea8bd
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-07-05 12:14:46 -0300

    Implements all() and getMedia() method #12

--- src/app/photos/photos-service.ts
+++ src/app/photos/photos-service.ts
@@ -3,5 +3,36 @@
 angular.module('confboilerplate.photos.services')
 
 .service('Photos', function($http) {
-  return {};
+  var API = {
+    BASE_URL: 'https://api.instagram.com/v1/tags/{tag}/media/recent',
+    GET_MEDIA: 'https://api.instagram.com/v1/media/{id}',
+    CLIENT_ID: '9f02a975012347d99daf55b3507b4f07', // your client id
+    COUNT: 20 // default limit by request
+  };
+
+  return {
+    all: function(tag, url) {
+      url = url || API.BASE_URL.replace(/(\{tag\})/g, tag);
+      var params = {
+        'params': {
+          'client_id': API.CLIENT_ID,
+          'count': API.COUNT,
+          'callback': 'JSON_CALLBACK'
+        }
+      };
+
+      return $http.jsonp(url, params);
+    },
+    getMedia: function(id) {
+      var url = API.GET_MEDIA.replace(/(\{id\})/g, id);
+      var params =  {
+        'params': {
+          'client_id': API.CLIENT_ID,
+          'CALLBACK': 'JSON_CALLBACK'
+        }
+      };
+
+      return $http.jsonp(url, params);
+    }
+  };
 });

3673da3a6805d6baf0a310813f9a1557c5dffe5c
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-27 03:24:56 -0300

    Add typescript task

---
+++ gulp/tasks/ts.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Necessary Plugins
+var gulp          = require('gulp')
+    ,paths          = require('../paths')
+    ,typescript     = require('gulp-typescript')
+    ,sourcemaps = require('gulp-sourcemaps');
+
+// Call Typescript Compiler
+module.exports = gulp.task('ts', function () {
+  return gulp.src(paths.source.ts)
+    .pipe(sourcemaps.init())
+    .pipe(typescript({
+      noImplicitAny:false,
+      sortOutput: true,
+      typescript: require('typescript')
+    }))
+    .pipe(sourcemaps.write('paths.build.sourceMaps'))
+    .pipe(gulp.dest('paths.build.html'));
+});

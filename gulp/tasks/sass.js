0da37f174c5207287c781bbcd3f94333b599cb96
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-27 03:25:13 -0300

    Add sass task

---
+++ gulp/tasks/sass.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Necessary Plugins
+var gulp     = require('gulp')
+    ,paths   = require('../paths')
+    ,sass    = require('gulp-sass')
+    ,minifyCss = require('gulp-minify-css')
+    ,rename = require('gulp-rename');
+
+// Call Sass
+module.exports = gulp.task('sass', function () {
+  return gulp.src(paths.source.files.sass)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest(paths.build.css))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest(paths.build.css));
+});

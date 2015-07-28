88e878a579365781935d6a37234835d6d9588e57
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-27 21:28:52 -0300

    Add paths to be used by BrowserSync

--- gulp/paths.js
+++ gulp/paths.js
@@ -3,18 +3,25 @@
 module.exports = {
 
     source: {
-        jade: './src/**/*.jade',
-        ts: './src/**/*.ts',
-        sass: './src/**/*.scss',
-        files: {
-            sass: './src/app/styles.scss'
+      jade: './src/**/*.jade',
+      ts: './src/**/*.ts',
+      sass: './src/**/*.scss',
+      files: {
+          sass: './src/app/styles.scss'
         }
 
     },
 
+    browserSync: {
+      html: 'www/**/*.html',
+      js: 'www/app/**/*.js',
+      css: 'www/assets/css/**/*.css',
+      img: 'www/assets/img/**/*'
+    },
+
     build: {
-        html: './www/',
-        css: './www/assets/css/',
-        sourceMaps: './'
+      html: './www/',
+      css: './www/assets/css/',
+      sourceMaps: './'
     }
 };
